539463e81cc08fb50b484012a096856e5c6d9c36
OriolRoura <53403172+OriolRoura@users.noreply.github.com>
2023-09-04 20:29:15 +0200

    Delete front-end directory

--- front-end/src/API/globals.js
+++
@@ -1,1 +0,0 @@
-export const conectionPath = "http://localhost:5000";
--- front-end/src/functionalities/markerEndStyle.js
+++
@@ -1,7 +0,0 @@
-import { MarkerType } from "reactflow";
-export const markerEndStyle = {
-    type: MarkerType.ArrowClosed,
-    color: '#000000',
-    width: 10,
-    height: 10,
-};
